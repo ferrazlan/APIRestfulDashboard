5cb0578ca9199c28ec44be2c9e7816cc021d5446
Fabio Ferraz Jorge <ferrazlan@hotmail.com>
2022-11-20 21:15:07 -0300

    API .NET5 atualizada e validada.

--- ApiDashboard/Context/AppDbContext.cs
+++ ApiDashboard/Context/AppDbContext.cs
@@ -11,7 +11,7 @@ namespace ApiDashboard.Context
 {
     public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-        DateTime dt_preview = DateTime.Now;
+        //DateTime dt_preview = DateTime.Now;
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -19,24 +19,24 @@ namespace ApiDashboard.Context
 
         public virtual DbSet<Card> TB_Card { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuider)
-        {
-            modelBuider.Entity<Card>().HasData(
-                new Card
-                {
-                    Id = 1,
-                    Title = "Criar Migration",
-                    Description = "Usar a branch master, fazer pull e iniciar a migração.",
-                    Date_Preview = "18/11/2022 00:00:00"
-                },
-                new Card
-                {
-                    Id = 2,
-                    Title = "Listagem Clientes",
-                    Description = "Colunas utilizadas: Código, Nome e Descrição do cliente.",
-                    Date_Preview = dt_preview.ToString()
-                }
-            );
-        }
+        //protected override void OnModelCreating(ModelBuilder modelBuider)
+        //{
+        //    modelBuider.Entity<Card>().HasData(
+        //        new Card
+        //        {
+        //            Id = 1,
+        //            Title = "Criar Migration",
+        //            Description = "Usar a branch master, fazer pull e iniciar a migração.",
+        //            Date_Preview = "18/11/2022 00:00:00"
+        //        },
+        //        new Card
+        //        {
+        //            Id = 2,
+        //            Title = "Listagem Clientes",
+        //            Description = "Colunas utilizadas: Código, Nome e Descrição do cliente.",
+        //            Date_Preview = dt_preview.ToString()
+        //        }
+        //    );
+        //}
     }
 }
